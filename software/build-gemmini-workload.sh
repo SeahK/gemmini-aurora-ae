1f02d43b1997426d3bb40d0c8db2ca2bbf49cb6d
Cloud User <centos@ip-192-168-1-213.ec2.internal>
2023-08-06 10:13:12 +0000

    update script

--- software/build-gemmini-workload.sh
+++ software/build-gemmini-workload.sh
@@ -9,7 +9,7 @@ RDIR=$(pwd)
 FSIMSW=/home/centos/firesim-aurora-ae/sw/firesim-software/
 cd gemmini-rocc-tests
 #./build.sh
-#cp -r build/* ../overlay/root/
+cp -r build/* ../overlay/root/
 cd $FSIMSW
 rm images/gemmini-tests-workload*
 ./marshal -v --workdir $RDIR build gemmini-tests-workload.json
