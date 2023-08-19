c354e09f2474f7abcf86eb7ce4e9b5d2a6d84c6b
Cloud User <centos@ip-192-168-1-213.ec2.internal>
2023-08-19 02:41:27 +0000

    oops

--- software/overlay/root/run-tests-noc.sh
+++ software/overlay/root/run-tests-noc.sh
@@ -1,16 +1,6 @@
 #!/usr/bin/env bash
 
 echo "*****************TEST RESULTS*************"
-
-echo "test_vel_set1"
-/root/imagenet/test_noc_vel1-linux
-echo "test_moca_set1"
-/root/imagenet/test_noc_moca1-linux
-echo "test_comp_set1"
-/root/imagenet/test_noc_comp1-linux
-echo "test_all_set1"
-/root/imagenet/test_noc_all1-linux
-
 echo "test_xr_vel"
 /root/imagenet/test_noc_xr_vel-linux
 echo "test_xr_moca"
@@ -20,6 +10,14 @@ echo "test_xr_comp"
 echo "test_xr_all"
 /root/imagenet/test_noc_xr_all-linux
 
+echo "test_vel_set1"
+/root/imagenet/test_noc_vel1-linux
+echo "test_moca_set1"
+/root/imagenet/test_noc_moca1-linux
+echo "test_comp_set1"
+/root/imagenet/test_noc_comp1-linux
+echo "test_all_set1"
+/root/imagenet/test_noc_all1-linux
 
 echo "test_vel_set2"
 /root/imagenet/test_noc_vel2-linux
--- software/overlay/root/run-tests-xbar.sh
+++ software/overlay/root/run-tests-xbar.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
 echo "*****************TEST RESULTS*************"
+echo "test_xr_vel"
+/root/imagenet/test_xr_vel-linux
+echo "test_xr_moca"
+/root/imagenet/test_xr_moca-linux
+echo "test_xr_comp"
+/root/imagenet/test_xr_comp-linux
+echo "test_xr_all"
+/root/imagenet/test_xr_all-linux
 
 echo "test_vel_set1"
 /root/imagenet/test_vel1-linux
@@ -10,15 +18,6 @@ echo "test_comp_set1"
 /root/imagenet/test_comp1-linux
 echo "test_all_set1"
 /root/imagenet/test_all1-linux
-
-echo "test_xr_vel"
-/root/imagenet/test_xr_vel-linux
-echo "test_xr_moca"
-/root/imagenet/test_xr_moca-linux
-echo "test_xr_comp"
-/root/imagenet/test_xr_comp-linux
-echo "test_xr_all"
-/root/imagenet/test_xr_all-linux
 
 echo "test_vel_set2"
 /root/imagenet/test_vel2-linux
